0a780d2abb5a7c5d37e0bb6011a0331ce64df8d1
Heinrich Latreider <heinrich.latreider@haw-hamburg.de>
2015-06-09 00:15:10 +0200

    adapter fertig

--- app/helpers/wp_backend_adapter.rb
+++ app/helpers/wp_backend_adapter.rb
@@ -16,9 +16,9 @@ class WPBackendAdapter
   @@backend_rest_uri = @@base_uri + 'rest/'
   @@backend_auth_uri = @@base_uri + 'oauth/'
 
-  @@username = 'test'
+  @@username = 'bicycleonrails'
   @@password = 'geheim'
-  @@UserId = 444
+  @@UserId = 5
 
   ## Auth-Token
   # holt sich ein aktuelles Auth-Token bom Backend
@@ -31,10 +31,8 @@ class WPBackendAdapter
   ## /routes/addRoute
   # fuegt dem Backend eine neue Route hinzu
   def self.create(track)
-
     track_ = {}
-    #return track.to_s
-    track_[:bezeichnung] = track[:name]
+    track_[:bezeichnung] = track[:name].to_s
 
     track_[:start] = track[:address][0]
     track_[:ende] = track[:address][track[:address].length - 1]
@@ -45,16 +43,12 @@ class WPBackendAdapter
         track_[:zwischenpunkte] << x
       end
     end
-    track_[:pois] = []
+    track_[:pois] = [{:bezeichnung => '', :info => '', :adresse => 'berliner tor 7 20099 hamburg'}]
     track_[:laenge] = "0"
 
-    #track_.to_s
-
-    options = {:body => track_.to_json, :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'}}
+    options = {:body => {:route => track_.to_json}, :headers => { 'Content-Type' => 'application/x-www-form-urlencoded', 'Accept' => 'application/json'}}
     result = HTTParty.post(@@backend_rest_uri+'routes/addRoute' + '?access_token=' + get_token, options)
-    #addRouteToBoR(result['id'],result['bezeichnung'])
-    #self.hash_to_route(result.parsed_response)
-    result
+    handleResult(result) {|route| route.each {|x| addRouteToBoR(x['id'],x['bezeichnung'])}}
   end
 
   ## /address/{address}
@@ -62,30 +56,45 @@ class WPBackendAdapter
   def self.validate_address(address)
     options = {}
     result = HTTParty.get(@@backend_rest_uri+'address/'+ URI.escape(address) + '?access_token=' + get_token, options)
-    if result['status'] == '200' and result['size'] > 0
-      true
-    else
-      false
+    result['status'] == '200' and result['size'] > 0
+  end
+
+  def self.getUserId()
+    options = {}
+    result = HTTParty.get(@@backend_rest_uri+'user/'+ @@username + '?access_token=' + get_token, options)
+    begin
+      res = handleResult(result) { |x| x[0]['id']}
+    rescue
+      signup()
+      result = HTTParty.get(@@backend_rest_uri+'user/'+ @@username + '?access_token=' + get_token, options)
+      res = result['responseValue'][0]['id']
     end
+    res
   end
 
   def self.signup
-    post = {:username => 'bicycleonrails',
-            :Forename => 'bicycleonrails',
-            :Surname => 'bicycleonrails',
-            :Emailaddress => 'bicycleonrails@haw-hamburg.de',
-            :Password => 'test',
-            :Street => 'Berliner Tor',
-            :Nr => '7',
-            :Plz => '20099',
-            :City => 'Hamburg',
-            :Country => 'Germany'}
+    post = {:username => @@username,
+            :password => 'test',
+            :forename => 'bicycleonrails',
+            :surname => 'bicycleonrails',
+            :emailAddress => 'bicycleonrailsathawhamburgde',
+            :street => 'BerlinerTor',
+            :housenumber => '7',
+            :plz => '20099',
+            :city => 'Hamburg',
+            :country => 'Germany'}
     options = {:body =>
-                   post.to_json,
-                :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'}}
-    result = HTTParty.post(@@backend_rest_uri+'user/newUser?access_token=' + get_token, options)
-    #handleResult(result)
-    #post.to_json
+                   post,
+                :headers => { 'Content-Type' => 'application/x-www-form-urlencoded', 'Accept' => 'application/json'}}
+    HTTParty.post(@@backend_rest_uri+'user/newUser?access_token=' + get_token, options)
+  end
+
+  ## /routes/getRoute
+  # gibt Route nach ID zurueck
+  def self.getRouteByID(id)
+    options = {:body => {:id => id}, :headers => { 'Content-Type' => 'application/x-www-form-urlencoded', 'Accept' => 'application/json'}}
+    result = HTTParty.post(@@backend_rest_uri+'/routes/getRoute?access_token=' + get_token, options)
+    handleResult(result) {|x| hash_to_route(x[0])}
   end
 
   ## /user/getRoutes
@@ -93,61 +102,69 @@ class WPBackendAdapter
   def self.getRoutesByUser(user)
     options = {}
     result = HTTParty.get(@@backend_rest_uri+'user/getRoutes/'+ URI.escape(user) + '?access_token=' + get_token, options)
-    #if result['status'] == '200' and result['size'] > 0
-    self.handleResult(result) {result.map {|x| hash_to_route(x)}}
-      #result.map {|x| self.hash_to_route(x)}
-    #elsif result['status'] == '403'
-    #  raise UnauthorizedError
-    #elsif result['status'] == '400' and result['size'] == 0
-    #  []
-    #else
-    #  raise UnhandledError
-    #end
+    handleResult(result) {|routes| routes.map { |x| hash_to_route(x)}}
   end
 
   ## /routes
   # sucht Routen nach Suchkriterium
   def self.searchRoute(criteria)
-    options = {:body => {:filterStr => criteria.to_s}, :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'}}
+    options = {:body => {:filterStr => criteria.to_s}, :headers => { 'Content-Type' => 'application/x-www-form-urlencoded', 'Accept' => 'application/json'}}
     result = HTTParty.post(@@backend_rest_uri+'routes' + '?access_token=' + get_token, options)
-    self.handleResult(result) {result.map {|x| hash_to_route(x)}}
-    #if result['status'] == '200' and result['size'] > 0
-    #  result
-    #elsif result['status'] == '403'
-    #  raise UnauthorizedError
-    #elsif result['size'] == 0
-    #  "no routes found"
-    #else
-    #  raise UnhandledError
-    #end
-
+    #result
+    self.handleResult(result) { |routes| routes.map {|x| hash_to_route(x)}}
   end
 
   ## /user/addFav
   # verknüpft Route mit Applikationsaccount
   def self.addRouteToBoR(idRoute, routename)
-    options = {:body => {:idUser => @@UserId, :username => @@username, :idRoute => idRoute, :routename => routename}.to_json, :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'}}
+    options = {:body => {iduser: @@UserId,
+                         username: @@username,
+                         idroute: idRoute,
+                         routename: routename},
+               :headers => { 'Content-Type' => 'application/x-www-form-urlencoded'}}
     result = HTTParty.post(@@backend_rest_uri+'user/addFav' + '?access_token=' + get_token, options)
-    result
+    handleResult(result) {|bool| bool[0]}
   end
 
+  ## /user/delFav
+  # entfernt Route vom Applikationsaccount
+  def self.deleteRouteFromBoR(idRoute, routename)
+    options = {:body => {iduser: @@UserId,
+                         username: @@username,
+                         idroute: idRoute,
+                         routename: routename},
+               :headers => { 'Content-Type' => 'application/x-www-form-urlencoded'}}
+    result = HTTParty.post(@@backend_rest_uri+'user/delFav' + '?access_token=' + get_token, options)
+    #handleResult(result) {|bool| bool[0]}
+  end
+
+  ##
+  # mappt die Backend-Route auf einheitliches Format
   def self.hash_to_route(x)
     route = Route.new()
-    route.id = x["id"]
-    route.name = x["bezeichnung"]
-    route.address = x["start"] ++ x["zwischenpunkte"] ++ x["ziel"]
+    route.id = x['id'].to_s
+    route.name = x['bezeichnung'].to_s
+    route.address = []
+    route.address << x['start']
+    x['zwischenpunkte'].each { |x| route.address << x }
+    route.address << x['ende']
     route
   end
 
+  ##
+  # behandelt den HTTP-Result
+  # wenn Status 403, dann unautorisiert
+  # wenn Status 400, dann keine Daten
+  # wenn Status 200 und mind. 1 Result in Array, dann wende Block auf result an
   def self.handleResult(result)
     if result['status'] == '200' and result['size'] > 0
-      yield(result)
+      yield(result['responseValue'])
     elsif result['status'] == '403'
       raise UnauthorizedError
     elsif result['status'] == '400' and result['size'] == 0
-      "no routes found"
+      "no data available"
     else
-      raise UnhandledError result.to_s
+      raise UnhandledError
     end
   end
 end
--- test/unit/wp_backend_adapter_test.rb
+++ test/unit/wp_backend_adapter_test.rb
@@ -9,4 +9,69 @@ class WPBackendAdapterTest < ActiveSupport::TestCase
   test "validate address" do
     assert_equal(WPBackendAdapter.validate_address("Berliner Tor 7"), true)
   end
+
+  test "userid" do
+    #WPBackendAdapter.getUserId
+    assert_equal(WPBackendAdapter.getUserId(), 5)
+  end
+
+  #test "create" do
+  #  WPBackendAdapter.create({:name => 'RailsRoute1', :address => ["Berliner tor 7 20099 hamburg","Berliner tor 30 20099 hamburg","Hamburger Strasse 1"]})
+  #end
+
+  test "getroute" do
+    route = WPBackendAdapter.getRouteByID(1)
+    assert_equal(1, route.id)
+    assert_equal('RailsRoute1', route.name)
+  end
+
+  test "getroutes by user" do
+    routes = WPBackendAdapter.getRoutesByUser('bicycleonrails')
+    assert_equal(1, routes.length)
+  end
+
+  test "hash to route" do
+    start = {
+        :id => 3,
+        :longitude => 10.06191,
+        :latitude => 53.66257,
+        :street => "Stofferkamp",
+        :housenumber => "75",
+        :plz => "22399",
+        :city => "Hamburg",
+        :country => "Germany",
+        :addressType => [:STREET_ADDRESS]}
+    ende = {
+        :id => 2,
+        :longitude => 9.99153,
+        :latitude => 53.55351,
+        :street => "Jungfernstieg",
+        :housenumber => "22",
+        :plz => "20354",
+        :city => "Hamburg",
+        :country => "Germany",
+        :addressType => [:STREET_ADDRESS]}
+    zwischenpunkt = {
+        :id => 6,
+        :longitude => 10.05974,
+        :latitude => 53.609848,
+        :street => "Cesar-Klein-Ring",
+        :housenumber => "28",
+        :plz => "22309",
+        :city => "Hamburg",
+        :country => "Germany",
+        :addressType => [:STREET_ADDRESS]}
+    backendRoute = {
+        'id' => 1,
+        'bezeichnung' => "Horny Route 66",
+        'start' => start,
+        'ende' => ende,
+        'zwischenpunkte' => [zwischenpunkt],
+        'pois' => [],
+        'laenge' => 25900}
+    mappedRoute = WPBackendAdapter.hash_to_route(backendRoute)
+    assert_equal(1, mappedRoute.id)
+    assert_equal("Horny Route 66", mappedRoute.name)
+    assert_equal([start,zwischenpunkt,ende], mappedRoute.address)
+  end
 end
